818d07f0a6d330e54808b08ab1b313b1e3881afa
Матвеец Андрей Вадимович <jokers.bit@gmail.com>
2022-02-28 04:26:20 +0300

    Fix some bugs at signatures finder

--- find_signatures.py
+++ find_signatures.py
@@ -3,15 +3,19 @@ from datetime import datetime
 from music21 import *
 from music21.chord import Chord
 from music21.note import Note
-from music21.stream import Stream
+from music21.stream import Stream, Part, Measure
 
 from benchmark.signature_benchmark import SignatureBenchmark
 from notes_utils import transpose_to_c
 
 # score1 = converter.parse('http://kern.ccarh.org/cgi-bin/ksdata?l=users/craig/classical/bach/cello&file=bwv1007-01.krn&f=kern')
 # score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?location=users/craig/classical/mozart/piano/sonata&file=sonata15-1.krn&format=kern')
-#score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?location=users/craig/classical/bach/371chorales&file=chor279.krn&f=kern')
-score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?l=users/craig/classical/mozart/piano/sonata&file=sonata10-1.krn&f=kern&o=norep')
+# score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?location=users/craig/classical/bach/371chorales&file=chor279.krn&f=kern')
+score1 = converter.parse(
+    'https://kern.humdrum.org/cgi-bin/ksdata?l=users/craig/classical/mozart/piano/sonata&file=sonata10-1.krn&f=kern&o=norep')
+
+
+# score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?location=users/craig/classical/chopin/prelude&file=prelude28-06.krn&format=kern')
 # score1.show()
 
 # todo фильтровать одинаковые сигнатуры - придумать как группировать одинаковые, привести сигнатуру к одному виду
@@ -19,14 +23,24 @@ score1 = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?l=users/craig/
 # todo обработать разные части
 # todo брать сигнатуры и потом проходить по произведениям смотреть где она там
 # todo отмечать сигнатуры в исходном произведении текстом или фоном
-# todo сразу добавлять в корзину со счетчиком, а не распределять после
+
+class IndexedNotes:
+
+    def __init__(self, notes, indexes):
+        self.notes = notes
+        self.indexes = indexes
+
+    def __eq__(self, o: object):
+        if isinstance(o, IndexedNotes):
+            return self.notes == o.notes and self.indexes == o.indexes
+        else:
+            return super().__eq__(o)
 
 
 class Signature:
 
-    def __init__(self, notes1, notes2):
-        self.notes1 = notes1
-        self.notes2 = notes2
+    def __init__(self, notes):
+        self.notes = notes
 
     def __str__(self):
         return self.get_note_str()
@@ -35,23 +49,25 @@ class Signature:
         return self.get_note_str()
 
     def __eq__(self, o: object):
-        return self.notes1 == o or self.notes2 == o
+        return self.notes == o
 
     def get_note_str(self):
-        return 'Found signature with: ' + ', '.join(str(n) for n in self.notes1) + ' and ' + ', '.join(str(n) for n in self.notes2)
+        return 'Found signature with: ' + ', '.join(str(n) for n in self.notes)
 
 
 class SignaturesFinder:
 
     def __init__(self,
-                 threshold=0,
-                 benchmark_percent=100,
+                 score=score1,
+                 threshold=20,
+                 benchmark_percent=80,
                  min_note_count=4,
                  max_note_count=10,
                  min_signature_entries=4,
                  max_signature_entries=10,
                  show_logs=True,
                  use_rhythmic=False):
+        self.score = score
         # допустимый процент несовпадения
         self.threshold = threshold
         # контрольный показатель совпадения, при котором тест считается пройденным
@@ -71,8 +87,9 @@ class SignaturesFinder:
 
     def __find_signatures(self):
         benchmark = SignatureBenchmark(self.benchmark_percent, self.threshold, self.use_rhythmic, self.show_logs)
-        intervals1 = self.__get_notes(transpose_to_c(score1))
-        intervals2 = self.__get_notes(transpose_to_c(score1))
+        transposed_score = transpose_to_c(self.score)
+        intervals1 = self.__get_notes(transposed_score)
+        intervals2 = self.__get_notes(transposed_score)
         result = []
 
         if len(intervals1) < len(intervals2):
@@ -81,29 +98,34 @@ class SignaturesFinder:
             intervals2 = temp_interval
 
         for i in range(0, len(intervals1)):
-            for k in range(i + self.min_note_count + 1, len(intervals2)):
+            for k in range(i + self.min_note_count, len(intervals2)):
                 j = i + self.min_note_count
                 m = k + self.min_note_count
-                signature = []
+                has_signature = False
                 notes1 = []
                 notes2 = []
-                while j < i + self.max_note_count and k + self.max_note_count > m and j < k:
-                    if 0 <= j < len(intervals1) and 0 <= m < len(intervals2):
+                while j <= i + self.max_note_count and k + self.max_note_count >= m and j <= k:
+                    if 0 <= j <= len(intervals1) and 0 <= m <= len(intervals2):
                         notes1 = intervals1[i: j]
                         notes2 = intervals2[k: m]
-                        if benchmark.is_signature(notes1, notes2):
-                            signature = notes2
+                        has_signature = benchmark.is_signature(notes1, notes2)
                     j += 1
                     m += 1
-                if len(signature) > 0:
-                    result.append(Signature(notes1, notes2))
+                if has_signature:
+                    signature = Signature([notes1, notes2])
+                    result.append(signature)
                     if self.show_logs:
-                        print('Found signature with: ', notes1, ' and ', notes2)
+                        print(signature)
         return result
 
     def __get_notes(self, score):
         notes = []
-        parts = [p.flat.notesAndRests.stream() for p in score.parts]
+        if isinstance(score, Part):
+            parts = [score.flat.notesAndRests.stream()]
+        elif isinstance(score, Measure):
+            parts = [score.flat.notesAndRests.stream()]
+        else:
+            parts = [p.flat.notesAndRests.stream() for p in score.parts]
         key = score.analyze('key')
         for note in parts[0]:
             if isinstance(note, Note):
@@ -124,13 +146,22 @@ class SignaturesFinder:
     def __count_repeated(self, list):
         list_with_count, counted_elements = [], []
         for element in list:
-            if not (element in counted_elements):
-                counted_elements = counted_elements + [element]
-                list_with_count = list_with_count + [[element, 1]]
-            else:
+            repeated = False
+            for counted_element in counted_elements:
+                has_notes = False
+                for notes in element.notes:
+                    if notes in counted_element.notes:
+                        has_notes = True
+                        repeated = True
+                    elif has_notes:
+                        counted_element.notes.append(notes)
+            if repeated:
                 for index in range(len(list_with_count)):
                     if list_with_count[index][0] == element:
                         list_with_count[index][1] = list_with_count[index][1] + 1
+            else:
+                counted_elements = counted_elements + [element]
+                list_with_count = list_with_count + [[element, 1]]
         return list_with_count
 
     def __filter_signatures_by_entries(self, list_with_count):
@@ -155,19 +186,27 @@ class SignaturesFinder:
         print('Filtered signatures by entries: ', len(filtered_signatures))
         for element in filtered_signatures:
             stream1 = Stream()
-            stream1.append(element.notes1)
-            stream1.append(note.Rest())
-            stream1.append(note.Rest())
-            stream1.append(note.Rest())
-            stream1.append(element.notes2)
+            for notes in element.notes:
+                stream1.append(notes)
+                stream1.append(note.Rest())
+                stream1.append(note.Rest())
+                stream1.append(note.Rest())
             # stream1.show()
-        print(*filtered_signatures, sep='\n')
+        result = []
+        for signature in filtered_signatures:
+            result.append(signature.notes)
+        return result
 
-
-SignaturesFinder().run()
+# print(*SignaturesFinder().run(), sep='\n')
 
 # K330
 # Time:  0:39:25.342773
 # Founded signatures len:  176454
 # Counted signatures len:  1458
 # Filtered signatures by entries:  249
+
+# K332
+# Time:  0:09:02.194635
+# Founded signatures len:  89788
+# Counted signatures len:  1046
+# Filtered signatures by entries:  382
