96b021d18880edf089d4ea7eb28639d50b848cdc
Матвеец Андрей Вадимович <jokers.bit@gmail.com>
2022-06-04 17:22:07 +0300

    Make other signatures search for composer finder and support parsing scores from file

--- find_composer.py
+++ find_composer.py
@@ -3,25 +3,21 @@ import json
 
 from music21 import converter
 
-from json_utils import NoteEncoder, note_decoder
-from notes_utils import transpose_to_c
+from json_utils import note_decoder
 from signatures_lsh import SignaturesFinder
 # багнутая - 'https://kern.humdrum.org/cgi-bin/ksdata?location=musedata/mozart/quartet&file=k161-01.krn&format=kern'
-score = converter.parse('https://kern.humdrum.org/cgi-bin/ksdata?location=musedata/mozart/quartet&file=k285-01.krn&format=kern')
 
 
 class ComposerFinder:
 
-    def __init__(self, score, database_paths):
-        expected_signatures = {}
+    def __init__(self, database_paths):
+        self.database_signatures = {}
         for path in database_paths:
             with open(path) as json_file:
-                expected_signatures.update(json.load(json_file, object_hook=note_decoder))
-        temp = []
+                self.database_signatures.update(json.load(json_file, object_hook=note_decoder))
         buckets = {}
-        # expected_signatures = collections.defaultdict(list)
         score_index_const = 100000
-        for i, el in enumerate(expected_signatures.items()):
+        for i, el in enumerate(self.database_signatures.items()):
             key, val = el
             for j, notes in enumerate(val[0]):
                 subvec = ','.join(map(str, notes))
@@ -37,18 +33,16 @@ class ComposerFinder:
             element = list(map(int, scores[0].split(',')))
             for indexes in scores[1]:
                 composer_index = int(indexes / score_index_const)
-                tup = list(expected_signatures.items())[composer_index]
+                tup = list(self.database_signatures.items())[composer_index]
                 tup[1][0].remove(element)
         print('Removed {} duplicates signatures from database'.format(len(to_remove)))
 
-        self.transposed_score = transpose_to_c(score)
-        self.transposed_notes = SignaturesFinder.__get_notes__(self.transposed_score)
-        notes = SignaturesFinder.__map_notes__(self.transposed_notes, False)
-        shingles = SignaturesFinder.getShingles(notes, 5)
+    def run(self, score):
+        shingles = sum(SignaturesFinder(score).run(), [])
         composer_count = {}
-        for composer in expected_signatures:
+        for composer in self.database_signatures:
             count = 0
-            signatures = expected_signatures[composer][0]
+            signatures = self.database_signatures[composer][0]
             for signature in signatures:
                 if signature in shingles:
                     count = count + 1
@@ -60,4 +54,13 @@ class ComposerFinder:
             print('{}: {} %'.format(composer, composer_count[composer] / signatures_count * 100))
 
 
-ComposerFinder(score, ['res/bach_short.json', 'res/mozart_short.json'])
+finder = ComposerFinder(['res/bach_short.json', 'res/mozart_short.json'])
+path = ''
+with open('res/scores/test_scores') as test_scores:
+    lines = test_scores.readlines()
+    for line in lines:
+        if line.startswith('#'):
+            continue
+        note_score = converter.parse(line.rstrip())
+        print('\nStarting search in ', line)
+        finder.run(note_score)
