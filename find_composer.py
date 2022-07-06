b2f58d7fddd18ff8a0daf81e5043472f7bc5c403
Andrey Matveets <matveets.av@gmail.com>
2022-07-07 01:01:37 +0300

    Support signatures highlighting and count all signatures entries

--- find_composer.py
+++ find_composer.py
@@ -1,14 +1,22 @@
 import json
+from collections import defaultdict
 
 from music21 import converter
 from music21 import environment
 
+import notes_utils
 from json_utils import note_decoder
 from notes_utils import should_skip
 from signatures_lsh import SignaturesFinder
 
-
-# багнутая - 'https://kern.humdrum.org/cgi-bin/ksdata?location=musedata/mozart/quartet&file=k161-01.krn&format=kern'
+composer_to_color = {'Mozart': '#00FFFF',
+                     'Schumann': '#FF00FF',
+                     'Beethoven': '#00FF00',
+                     'Haydn': '#0000FF',
+                     'Tchaikovsky': '#FF0000',
+                     'Vivaldi': '#FFFF00',
+                     'Bach': '#800080',
+                     'Schubert': '#FBA0E3'}
 
 
 class ComposerFinder:
@@ -20,6 +28,7 @@ class ComposerFinder:
                 self.database_signatures.update(json.load(json_file, object_hook=note_decoder))
         buckets = {}
         score_index_const = 100000
+        count = 0
         for i, el in enumerate(self.database_signatures.items()):
             key, val = el
             for j, notes in enumerate(val[0]):
@@ -28,19 +37,26 @@ class ComposerFinder:
                 if subvec not in buckets:
                     buckets[subvec] = []
                 buckets[subvec].append(composer_index)
+                count += 1
         to_remove = []
         for el in buckets.items():
             if len(el[1]) > 1:
                 to_remove.append(el)
+
         for scores in to_remove:
             element = list(map(int, scores[0].split(',')))
             for indexes in scores[1]:
                 composer_index = int(indexes / score_index_const)
                 tup = list(self.database_signatures.items())[composer_index]
                 tup[1][0].remove(element)
-        print('Removed {} duplicates signatures from database'.format(len(to_remove)))
+        print('Removed {} duplicates signatures from database of {}'.format(len(to_remove), count))
 
     def run(self, score):
+        transposed_score = notes_utils.transpose_to_c(score)
+        transposed_notes = SignaturesFinder.__get_notes__(transposed_score)
+        mapped_notes = SignaturesFinder.__map_notes__(transposed_notes, False)
+        original_notes = SignaturesFinder.__pick_notes_from_score__(transposed_score)[0]
+
         shingles = []
         for i in range(6, 11):
             shingles.append(sum(SignaturesFinder(score, min_note_count=i).run(), []))
@@ -51,18 +67,36 @@ class ComposerFinder:
             signatures = self.database_signatures[composer][0]
             for signature in signatures:
                 if signature in shingles:
+                    print('Composer {} has signature: {}'.format(composer, str(signature)))
                     count = count + 1
+                    index = find_index(mapped_notes, signature)
+                    for note in range(index, index + len(signature)):
+                        original_notes[note].style.color = composer_to_color[composer]
             if count > 0:
                 composer_count[composer] = count
         signatures_count = sum(composer_count.values())
-        for composer in composer_count:
-            print('{}: {} %'.format(composer, composer_count[composer] / signatures_count * 100))
         composer_to_result = {}
         for composer in composer_count:
             composer_to_result[composer] = {'count': composer_count[composer],
                                             'confidence': composer_count[composer] / signatures_count * 100}
         print(composer_to_result)
+        # transposed_score.show()
         return composer_to_result
+
+
+def find_index(transposed_notes, shingle):
+    index = 0
+    shingle_index = 0
+    while index < len(transposed_notes):
+        if shingle_index == len(shingle):
+            return index - len(shingle)
+        if transposed_notes[index] == shingle[shingle_index]:
+            index += 1
+            shingle_index += 1
+        else:
+            index += 1
+            shingle_index = 0
+    return -1
 
 
 def get_composer_with_max(finder_result):
@@ -79,17 +113,18 @@ def get_composer_with_max(finder_result):
 e = environment.Environment()
 e['autoDownload'] = 'allow'
 
-finder = ComposerFinder(['res/scores/cortical-algorithms/bach-control-set.json',
-                         'res/scores/cortical-algorithms/beethoven-control-set.json',
-                         'res/scores/cortical-algorithms/chopin-control-set-1.json',
-                         'res/scores/cortical-algorithms/corelli-control-set.json',
-                         'res/scores/cortical-algorithms/haydn-control-set.json',
-                         'res/scores/cortical-algorithms/joplin-control-set-1.json',
-                         'res/scores/cortical-algorithms/mozart-control-set.json',
-                         'res/scores/cortical-algorithms/scarlatti-control-set.json',
-                         'res/scores/cortical-algorithms/vivaldi-control-set-1.json',
-                         ])
-input_path = 'res/scores/cortical-algorithms/mozart-control-set-testing-set.json'
+finder = ComposerFinder([
+    'res/scores/markov-chains/bach-control-set.json',
+    'res/scores/markov-chains/beethoven-control-set.json',
+    'res/scores/language-models/haydn.json',
+    'res/scores/markov-chains/mozart-control-set.json',
+    'res/scores/cortical-algorithms/vivaldi-control-set-1.json',
+    'res/scores/n-grams/schubert-control-set.json',
+    'res/dataset/schumann/schumann.json',
+    'res/dataset/tchaikovsky/tchaikovsky.json',
+    'res/dataset/tchaikovsky/tchaikovsky-2.json'
+])
+input_path = 'res/dataset/tchaikovsky/child_album.json'
 
 with open(input_path) as test_scores:
     comp_to_scores = json.load(test_scores)
@@ -115,6 +150,11 @@ with open(input_path) as test_scores:
         correct_results += current_correct
         print('Got {}% for {}'.format(current_correct / len(comp_to_scores[composer]) * 100, composer))
     print('Finally got {}% for all'.format(correct_results / score_count * 100))
+    counts = defaultdict(lambda: 0)
+    for score in result:
+        for composer in result[score]:
+            counts[composer] += result[score][composer]['count']
+    print(counts)
 
     with open(input_path + "-composer-result.json", "w") as outfile:
         json.dump(result, outfile)
